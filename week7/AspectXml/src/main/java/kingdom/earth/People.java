54c8fcdd4aadae05b270a375af8f162a3c099152
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-02 11:49:17 +0200

    Remove annotations from xml method
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- week7/AspectXml/src/main/java/kingdom/earth/People.java
+++ week7/AspectXml/src/main/java/kingdom/earth/People.java
@@ -7,18 +7,12 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Aspect
 public class People {
 
-    @Pointcut("execution(* kingdom.Knight.saveKingdom(..))")
-    public void kingdomSaving() {}
-
-    @Before("kingdomSaving()")
     public static void sayGoodLuck() {
         System.out.println("People: Good luck, knight!");
     }
 
-    @After("kingdomSaving()")
     public static void sayThankYou() {
         System.out.println("People: Thank you, knight!");
     }
