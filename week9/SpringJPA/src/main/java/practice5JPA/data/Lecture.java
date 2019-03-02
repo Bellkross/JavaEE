388dd19b132307d1a9ebcd83bdc33996574fc08a
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-02 15:23:07 +0200

    Fix database path, create Lecture object

---
+++ week9/SpringJPA/src/main/java/practice5JPA/data/Lecture.java
@@ -0,0 +1,39 @@
+package practice5JPA.data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Lectures")
+public class Lecture {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private int id;
+    private String name;
+    private double credits;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(final double credits) {
+        this.credits = credits;
+    }
+}
