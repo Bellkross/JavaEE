dd7b815760487eaa3f46b32e1cc66649319c5b79
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-02 15:25:17 +0200

    Create lectures worker

---
+++ week9/SpringJPA/src/main/java/practice5JPA/data/LecturesWorker.java
@@ -0,0 +1,15 @@
+package practice5JPA.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LecturesWorker {
+
+    @Autowired
+    LecturesDao lecturesDao;
+    public Lecture addLecture(Lecture lecture){
+        lecture = lecturesDao.addLecture(lecture);
+        System.out.println(lecture);
+        return lecture;
+    }
+}
+
